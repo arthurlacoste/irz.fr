05f0a5b4f86a4abc4dd68cfed1fd8b47d74ded49
Arthur Lacoste <arthurlacoste@users.noreply.github.com>
2017-11-15 21:24:19 +0100

    Fix js for support all(d) browsers

--- assets/js/plugins/jquery.greedy-navigation.js
+++ assets/js/plugins/jquery.greedy-navigation.js
@@ -4,72 +4,75 @@ Licensed under the MIT license - http://opensource.org/licenses/MIT
 Copyright (c) 2015 Luke Jackson
 */
 
-$(document).ready(() => {
-	const $btn = $('nav.greedy-nav button');
-	const $vlinks = $('nav.greedy-nav .visible-links');
-	const $hlinks = $('nav.greedy-nav .hidden-links');
+$(document).ready(function(){
 
-	let numOfItems = 0;
-	let totalSpace = 0;
-	const closingTime = 1000;
-	const breakWidths = [];
+  var $btn = $('nav.greedy-nav button');
+  var $vlinks = $('nav.greedy-nav .visible-links');
+  var $hlinks = $('nav.greedy-nav .hidden-links');
+
+  var numOfItems = 0;
+  var totalSpace = 0;
+  var closingTime = 1000;
+  var breakWidths = [];
 
   // Get initial state
-	$vlinks.children().outerWidth((i, w) => {
-		totalSpace += w;
-		numOfItems += 1;
-		breakWidths.push(totalSpace);
-	});
+  $vlinks.children().outerWidth(function(i, w) {
+    totalSpace += w;
+    numOfItems += 1;
+    breakWidths.push(totalSpace);
+  });
 
-	let availableSpace, numOfVisibleItems, requiredSpace, timer;
+  var availableSpace, numOfVisibleItems, requiredSpace, timer;
 
-	function check() {
+  function check() {
+
     // Get instant state
-		availableSpace = $vlinks.width() - 10;
-		numOfVisibleItems = $vlinks.children().length;
-		requiredSpace = breakWidths[numOfVisibleItems - 1];
+    availableSpace = $vlinks.width() - 10;
+    numOfVisibleItems = $vlinks.children().length;
+    requiredSpace = breakWidths[numOfVisibleItems - 1];
 
     // There is not enough space
-		if (requiredSpace > availableSpace) {
-			$vlinks.children().last().prependTo($hlinks);
-			numOfVisibleItems -= 1;
-			check();
+    if (requiredSpace > availableSpace) {
+      $vlinks.children().last().prependTo($hlinks);
+      numOfVisibleItems -= 1;
+      check();
       // There is more than enough space
-		} else if (availableSpace > breakWidths[numOfVisibleItems]) {
-			$hlinks.children().first().appendTo($vlinks);
-			numOfVisibleItems += 1;
-			check();
-		}
+    } else if (availableSpace > breakWidths[numOfVisibleItems]) {
+      $hlinks.children().first().appendTo($vlinks);
+      numOfVisibleItems += 1;
+      check();
+    }
     // Update the button accordingly
-		$btn.attr('count', numOfItems - numOfVisibleItems);
-		if (numOfVisibleItems === numOfItems) {
-			$btn.addClass('hidden');
-		} else {
-			$btn.removeClass('hidden');
-		}
-	}
+    $btn.attr("count", numOfItems - numOfVisibleItems);
+    if (numOfVisibleItems === numOfItems) {
+      $btn.addClass('hidden');
+    } else {
+      $btn.removeClass('hidden');
+    }
+  }
 
   // Window listeners
-	$(window).resize(() => {
-		check();
-	});
+  $(window).resize(function() {
+    check();
+  });
 
-	$btn.on('click', function () {
-		$hlinks.toggleClass('hidden');
-		$(this).toggleClass('close');
-		clearTimeout(timer);
-	});
+  $btn.on('click', function() {
+    $hlinks.toggleClass('hidden');
+    $(this).toggleClass('close');
+    clearTimeout(timer);
+  });
 
-	$hlinks.on('mouseleave', () => {
+  $hlinks.on('mouseleave', function() {
     // Mouse has left, start the timer
-		timer = setTimeout(() => {
-			$hlinks.addClass('hidden');
-			$btn.toggleClass('close');
-		}, closingTime);
-	}).on('mouseenter', () => {
+    timer = setTimeout(function() {
+      $hlinks.addClass('hidden');
+      $btn.toggleClass('close');
+    }, closingTime);
+  }).on('mouseenter', function() {
     // Mouse is back, cancel the timer
-		clearTimeout(timer);
-	});
+    clearTimeout(timer);
+  })
 
-	check();
+  check();
+
 });
